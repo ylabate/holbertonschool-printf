4932663ddcae4734c2dff7a2957a07d8ce4598a5
ylabate <12196@holbertonstudents.com>
2025-11-24 15:59:24 +0000

    Refactor main function for testing and adjust return value in _printf

--- main.c
+++ main.c
@@ -9,14 +9,9 @@
  */
 int main(void)
 {
+	int a;
 
-	_printf("Let's try to printf a simple sentence.\n");
-	printf("Let's try to printf a simple sentence.\n");
-	_printf("Character:[%c]\n", 'H');
-	printf("Character:[%c]\n", 'H');
-	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
-	_printf("Percent:[%%]\n");
-	printf("Percent:[%%]\n");
+	a =_printf("%c", 'S');
+	printf("%d", a);
 	return (0);
 }

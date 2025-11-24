3382a1aa59d58141066b7cc1e13d7352d5007ce0
ylabate <12196@holbertonstudents.com>
2025-11-24 15:35:53 +0000

    Remove unused variable and commented code in main function

--- main.c
+++ main.c
@@ -9,13 +9,14 @@
  */
 int main(void)
 {
-	char a = 'b';
 
-	_printf("aa%caa\n", a);
-/**
-*	_printf("Character:[%c]\n", 'H');
-*	_printf("String:[%s]\n", "I am a string !");
-*/
+	_printf("Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Percent:[%%]\n");
+	printf("Percent:[%%]\n");
 	return (0);
 }
-

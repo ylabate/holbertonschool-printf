71fa19156b669bdfbdd79de66e789e190d067570
ylabate <12196@holbertonstudents.com>
2025-11-25 13:54:17 +0000

    fix: refactor print_integer function to correctly handle integer printing

--- print_integer.c
+++ print_integer.c
@@ -3,22 +3,32 @@
 int print_integer(va_list list)
 {
 	int integer = va_arg(list, int);
-	int len = 0;
+	int len = 0, rev_len;
 	int buffer_index = 0;
-	char *buffer[12];
+	char buffer[12];
 
 	if (integer < 0)
 	{
-		*buffer[buffer_index++] = '-';
+		buffer[buffer_index++] = '-';
 		integer = -integer;
+		len++;
+	}
+	if (integer == 0)
+	{
+		buffer[0] = '0';
 		len++;
 	}
 	while (integer > 0)
 	{
-		*buffer[buffer_index++] = ((integer % 10) + '0');
+		buffer[buffer_index++] = ((integer % 10) + '0');
 		integer = (integer / 10);
 		len++;
 	}
-	write(1, buffer, len);
+	rev_len = len;
+	while (rev_len >= 0)
+	{
+		write(1, &buffer[rev_len], 1);
+		rev_len--;
+	}
 	return (len);
 }

2107c1334e17f53fbb6302cc1bece7568044ad80
ylabate <12196@holbertonstudents.com>
2025-11-25 14:45:56 +0000

    fix: update main function to print INT_MIN and refactor print_integer to handle negative integers correctly

--- print_integer.c
+++ print_integer.c
@@ -3,26 +3,27 @@
 int print_integer(va_list list)
 {
 	int integer = va_arg(list, int);
+	long signed int integer_pos;
 	int end = 0, rev_end, len = 0;
 	int buffer_index = 0;
 	char buffer[12];
 
-	if (integer < 0)
-	{
-		write(1, "-", 1);
-		len++;
-		integer = -integer;
-	}
+	integer_pos = integer;
 	if (integer == 0)
 	{
 		buffer[0] = '0';
 		end++;
-		len++;
 	}
-	while (integer > 0)
+	if (integer < 0)
 	{
-		buffer[buffer_index++] = ((integer % 10) + '0');
-		integer = (integer / 10);
+		write(1, "-", 1);
+		len++;
+		integer_pos = -integer_pos;
+	}
+	while (integer_pos > 0)
+	{
+		buffer[buffer_index++] = ((integer_pos % 10) + '0');
+		integer_pos = (integer_pos / 10);
 		end++;
 	}
 	rev_end = end - 1;

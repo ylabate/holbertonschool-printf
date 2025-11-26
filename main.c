4b9e947a52525f602132a22a6320db43bd9b8170
ylabate <12196@holbertonstudents.com>
2025-11-26 10:35:26 +0000

    fix: refactor _printf to correctly accumulate printed character count

--- main.c
+++ main.c
@@ -13,68 +13,104 @@ int main(void)
 	char c = 'A';
 	int num = 42;
 	int neg = -123;
+	int count_printf, count__printf;
 
 	printf("=== Tests de _printf vs printf ===\n\n");
 
 	/* Test %s */
 	printf("Test %%s avec chaîne normale:\n");
-	printf("printf:  %s\n", str);
-	_printf("_printf: %s\n", str);
+	count_printf = printf("printf:  %s\n", str);
+	count__printf = _printf("_printf: %s\n", str);
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	printf("Test %%s avec chaîne vide:\n");
-	printf("printf:  %s\n", "");
-	_printf("_printf: %s\n", "");
+	count_printf = printf("printf:  %s\n", "");
+	count__printf = _printf("_printf: %s\n", "");
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	/* Test %c */
 	printf("Test %%c:\n");
-	printf("printf:  %c\n", c);
-	_printf("_printf: %c\n", c);
+	count_printf = printf("printf:  %c\n", c);
+	count__printf = _printf("_printf: %c\n", c);
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	/* Test %% */
 	printf("Test %%%%:\n");
-	printf("printf:  %%\n");
-	_printf("_printf: %%\n");
+	count_printf = printf("printf:  %%\n");
+	count__printf = _printf("_printf: %%\n");
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	/* Test %i */
 	printf("Test %%i avec nombre positif:\n");
-	printf("printf:  %i\n", num);
-	_printf("_printf: %i\n", num);
+	count_printf = printf("printf:  %i\n", num);
+	count__printf = _printf("_printf: %i\n", num);
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	printf("Test %%i avec nombre négatif:\n");
-	printf("printf:  %i\n", neg);
-	_printf("_printf: %i\n", neg);
+	count_printf = printf("printf:  %i\n", neg);
+	count__printf = _printf("_printf: %i\n", neg);
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	printf("Test %%i avec INT_MIN:\n");
-	printf("printf:  %i\n", INT_MIN);
-	_printf("_printf: %i\n", INT_MIN);
+	count_printf = printf("printf:  %i\n", INT_MIN);
+	count__printf = _printf("_printf: %i\n", INT_MIN);
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	/* Test %d  (même que %i) */
 	printf("Test %%d avec nombre positif:\n");
-	printf("printf:  %d\n", num);
-	_printf("_printf: %d\n", num);
+	count_printf = printf("printf:  %d\n", num);
+	count__printf = _printf("_printf: %d\n", num);
+	if (count_printf == count__printf)
+		printf("Counts match: %d\n", count_printf);
+	else
+		printf("Counts differ: printf=%d, _printf=%d\n", count_printf, count__printf);
 	printf("\n");
 
 	/* Test %b  (binaire, non standard dans printf) */
 	printf("Test %%b avec nombre positif:\n");
 	printf("printf:  N/A (%%b non standard)\n");
-	_printf("_printf: %b\n", num);
+	count__printf = _printf("_printf: %b\n", num);
+	printf("_printf count: %d\n", count__printf);
 	printf("\n");
 
 	printf("Test %%b avec nombre négatif:\n");
 	printf("printf:  N/A\n");
-	_printf("_printf: %b\n", neg);
+	count__printf = _printf("_printf: %b\n", neg);
+	printf("_printf count: %d\n", count__printf);
 	printf("\n");
 
 	printf("Test %%b avec INT_MIN:\n");
 	printf("printf:  N/A\n");
-	_printf("_printf: %b\n", INT_MIN);
+	count__printf = _printf("_printf: %b\n", INT_MIN);
+	printf("_printf count: %d\n", count__printf);
 	printf("\n");
 
 	printf("=== Fin des tests ===\n");

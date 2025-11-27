141cee9df2c31674a1e1a4d51569efadf4da5100
ylabate <12196@holbertonstudents.com>
2025-11-27 15:10:15 +0000

    fix: correct hexadecimal representation in print_adresse function

--- print_adresse.c
+++ print_adresse.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
+/**
+ * print_adresse - Prints the address of a pointer in hexadecimal format.
+ * @list: va_list containing the pointer to print.
+ * @buffer: Buffer to store the resulting string.
+ *
+ * Return: Number of characters written to buffer.
+ */
 int print_adresse(va_list list, char *buffer)
 {
 	void *pointer = va_arg(list, void *);
 	size_t adresse;
 	int buffer_index = 0, local_buffer_index = 0, end = 0, len = 0;
-	char hexa[] = {"0123456789ABCDEF"};
+	char hexa[] = {"0123456789abcdef"};
 	char local_buffer[32];
 	char null_str[] = {"(nil)"};
 	int null_index = 0;
